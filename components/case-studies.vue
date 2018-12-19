953ebd9de48806d2f10512abc151381b459a4da9
Brandon Morse <brandon@morsecodemedia.com>
2018-12-19 12:05:57 -0500

    working through a shared style and dom for case studies regardless if they are full listings or features. and setting up the nuxt-router links to get to the detail pages of each respective listing.

--- components/case-studies.vue
+++ components/case-studies.vue
@@ -1,8 +1,6 @@
 <template>
-  <section
-    v-if="caseStudies"
-    class="case-studies-listing">
-    <nav>
+  <section>
+    <nav class="case-studies-filter">
       <ul>
         <li>Alexa Skills</li>
         <li>Conference Panels</li>
@@ -12,24 +10,25 @@
         <li>Websites</li>
       </ul>
     </nav>
-    <nuxt-link
-      v-for="(cs, index) in featuredCaseStudies"
-      :key="index"
-      :to="cs.title | lowerKebab">
-      <article
-        :class="cs.category | lowerKebab">
-        <img
-          :src="cs.previewImg"
-          role="presentation">
-        <div>
+    <article
+      v-if="caseStudies"
+      class="case-studies">
+      <nuxt-link
+        v-for="(cs, index) in allCaseStudies"
+        :key="index"
+        :to="'/case-studies/' + cs.title | lowerKebab">
+        <div
+          :class="(index === 0) ? 'big' : (index === 4) ? 'horizontal' : ''"
+          class="case-study">
           <img
-            :src="cs.brandLogo"
+            :src="cs.previewImg"
             role="presentation">
-          <h3>{{ cs.title }}</h3>
-          <p>{{ cs.description }}</p>
+          <div class="overlay">
+            <h3>{{ cs.title }}</h3>
+          </div>
         </div>
-      </article>
-    </nuxt-link>
+      </nuxt-link>
+    </article>
   </section>
 </template>
 
@@ -48,7 +47,7 @@
       }
     },
     computed : {
-      featuredCaseStudies() {
+      allCaseStudies() {
         let activeProjects = this.caseStudies
           .filter(caseStudy => caseStudy.yearEnd === '' && caseStudy.active)
         let sortedCaseStudies = this.caseStudies
@@ -70,108 +69,6 @@
   }
 </script>
 
-<style lang="scss" scoped>
-  .case-studies-listing {
-    a {
-      text-decoration: none;
-      h3,
-      p {
-        text-decoration: none;
-      }
-      &:nth-of-type(odd) {
-        article {
-          grid-template-areas:
-          "i i i i i i i a a a a a"
-          "i i i i i i i a a a a a"
-          "i i i i i i i a a a a a"
-          "i i i i i i i a a a a a"
-          "i i i i i i i a a a a a"
-          "i i i i i i i a a a a a";
-        }
-      }
-
-      &:nth-of-type(even) {
-        article {
-          grid-template-areas:
-          "a a a a a i i i i i i i"
-          "a a a a a i i i i i i i"
-          "a a a a a i i i i i i i"
-          "a a a a a i i i i i i i"
-          "a a a a a i i i i i i i"
-          "a a a a a i i i i i i i";
-        }
-      }
-    }
-
-    aside {
-      ul {
-        text-align: center;
-        li {
-          display: inline-block;
-          margin: 0 5px;
-          padding: 0 10px;
-        }
-      }
-    }
-
-    article {
-      display: grid;
-      grid-template-columns: repeat(12, 1fr);
-      grid-template-rows: repeat(6, 60px);
-      margin: 0 0 10px 0;
-
-      &:hover {
-        background: antiquewhite;
-      }
-
-      > img {
-        grid-area: i;
-        object-fit: cover;
-        width: 100%;
-        height: 100%;
-      }
-
-      > div {
-        grid-area: a;
-        display: grid;
-        grid-template-columns: repeat(12, 1fr);
-        grid-template-rows: repeat(6, 60px);
-        grid-template-areas:
-        ". . . . . . . . . . . ."
-        ". I I I I I I I I I I ."
-        ". . . . . . . . . . . ."
-        ". T T T T T T T T T T ."
-        ". D D D D D D D D D D ."
-        ". . . . . . . . . . . .";
-
-        > img {
-          grid-area: I;
-          justify-self: center;
-        }
-
-        > h3 {
-          grid-area: T;
-          text-align: center;
-        }
-
-        > p {
-          grid-area: D;
-          text-align: center;
-        }
-      }
-    }
-  }
-
-  @media (min-width: 576px) {
-
-  }
-
-  @media (min-width: 768px) {
-
-  }
-
-  @media (min-width: 1200px) {
-
-  }
+<style lang="scss">
 
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -4,52 +4,153 @@
   </div>
 </template>
 
-<style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
+<style lang="scss">
+  .case-studies {
+    display: grid;
+    grid-gap: 5px;
+    grid-template-columns: repeat(auto-fit, minmax(153px, 1fr));
+    grid-auto-rows: 75px;
+    grid-auto-flow: dense;
+    a {
+      > div.case-study {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        position: relative;
+        > img {
+          object-fit: cover;
+          width: 100%;
+          height: 100%;
+          -webkit-transition: all 0.35s;
+          transition: all 0.35s;
+        }
+        .overlay {
+          position: absolute;
+          overflow: hidden;
+          width: 80%;
+          height: 80%;
+          left: 10%;
+          top: 10%;
+          border-bottom: 1px solid #FFF;
+          border-top: 1px solid #FFF;
+          -webkit-transition: opacity 0.35s, -webkit-transform 0.35s;
+          transition: opacity 0.35s, transform 0.35s;
+          -webkit-transform: scale(0,1);
+          -ms-transform: scale(0,1);
+          transform: scale(0,1);
+          h3 {
+            text-transform: uppercase;
+            text-align: center;
+            position: relative;
+            justify-self: center;
+            font-size: 22px;
+            background-color: transparent;
+            color: #FFF;
+            padding: 1em 0;
+            opacity: 0;
+            filter: alpha(opacity=0);
+            -webkit-transition: opacity 0.35s, -webkit-transform 0.35s;
+            transition: opacity 0.35s, transform 0.35s;
+            -webkit-transform: translate3d(0,-100%,0);
+            transform: translate3d(0,-100%,0);
+          }
+          a,
+          p {
+            color: #FFF;
+            padding: 1em 0;
+            opacity: 0;
+            filter: alpha(opacity=0);
+            -webkit-transition: opacity 0.35s, -webkit-transform 0.35s;
+            transition: opacity 0.35s, transform 0.35s;
+            -webkit-transform: translate3d(0,100%,0);
+            transform: translate3d(0,100%,0);
+          }
+        }
+        &:hover {
+          cursor: pointer;
+          .overlay {
+            opacity: 1;
+            filter: alpha(opacity=100);
+            -webkit-transform: scale(1);
+            -ms-transform: scale(1);
+            transform: scale(1);
+            display: flex;
+            align-items: center;
+            justify-content: center;
+          }
+          img {
+            filter: url('data:image/svg+xml;charset=utf-8,<svg xmlns="http://www.w3.org/2000/svg"><filter id="filter"><feComponentTransfer color-interpolation-filters="sRGB"><feFuncR type="linear" slope="0.6" /><feFuncG type="linear" slope="0.6" /><feFuncB type="linear" slope="0.6" /></feComponentTransfer></filter></svg>#filter');
+            filter: brightness(0.6);
+            -webkit-filter: brightness(0.6);
+          }
+          a,
+          p,
+          h3 {
+            opacity: 1;
+            filter: alpha(opacity=100);
+            -webkit-transform: translate3d(0,0,0);
+            transform: translate3d(0,0,0);
+          }
+        }
+      }
+    }
+    .big {
+      grid-column: span 2;
+      grid-row: span 2;
+    }
+  }
+  .link-bar {
+    background: white;
+    padding: 45px 25px;
+    a {
+      color: #222;
+      font-size: 24px;
+      text-decoration: none;
+      text-transform: uppercase;
+    }
+  }
+  nav {
+    &.case-studies-filter {
+      ul {
+        text-align: center;
+        li {
+          display: inline-block;
+          margin: 0 5px;
+          padding: 0 10px;
+        }
+      }
+    }
+  }
+  @media (min-width: 576px) {
+    .case-studies {
+      grid-template-columns: repeat(auto-fit, minmax(253px, 1fr));
+      grid-auto-rows: 126px;
+    }
+  }
 
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
-}
+  @media (min-width: 768px) {
+    .case-studies {
+      grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+      grid-auto-rows: 250px;
+    }
+  }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
+  @media (min-width: 1200px) {
+    .case-studies {
+      .horizontal {
+        grid-column: span 1;
+        grid-row: span 1;
+      }
+    }
+  }
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
+  @media (min-width: 910px) and (max-width: 1214px) {
+    .case-studies {
+      .horizontal {
+        grid-column: span 2;
+        grid-row: span 1;
+      }
+    }
+  }
 </style>
