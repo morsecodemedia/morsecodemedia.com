8c51326e5667afb2ba812fb72c342794acdc37af
Brandon Morse <brandon@morsecodemedia.com>
2018-12-19 10:52:20 -0500

    removing all files from the legacy processwire site.

--- site/templates/T02.0-Who-I-Am.php
+++
@@ -1,6 +0,0 @@
-<?php include_once($_SERVER['DOCUMENT_ROOT'].'/site/templates/embeds/header.php'); ?>
-<?php include_once($_SERVER['DOCUMENT_ROOT'].'/site/templates/modules/interior-hero.php'); ?>
-<?php include_once($_SERVER['DOCUMENT_ROOT'].'/site/templates/modules/about-me-intro.php'); ?>
-<?php include_once($_SERVER['DOCUMENT_ROOT'].'/site/templates/modules/work-experience.php'); ?>
-<?php include_once($_SERVER['DOCUMENT_ROOT'].'/site/templates/modules/certifications.php'); ?>
-<?php include_once($_SERVER['DOCUMENT_ROOT'].'/site/templates/embeds/footer.php'); ?>
