6b90ee5d750834bf0420b4c6aff15b0494830bfa
Brandon Morse <brandon@morsecodemedia.com>
2019-01-04 08:32:12 -0500

    tweaked navigation styles

--- components/header.vue
+++ components/header.vue
@@ -1,6 +1,6 @@
 <template>
-  <section>
-    <header>
+  <header>
+    <div class="masthead">
       <div class="logo">
         <a href="/">&lt;morsecodemedia&gt;</a>
       </div>
@@ -15,7 +15,7 @@
         </div>
         <div class="menu-label">Menu</div>
       </div>
-    </header>
+    </div>
     <nav
       v-if="navigation.length > 0"
       class="main-nav">
@@ -30,7 +30,7 @@
         </li>
       </ul>
     </nav>
-  </section>
+  </header>
 </template>
 
 <script>
@@ -66,114 +66,134 @@
     top: 0;
     left: 0;
     z-index: 1;
-    background: #222;
-    display: grid;
-    grid-template-columns: repeat(12, 1fr);
-    grid-template-rows: auto auto;
-    grid-template-areas:
-    "logo logo logo logo logo logo logo logo logo logo logo mbtn";
-    .logo {
+    .masthead {
       background: #222;
-      color: white;
-      font-size: 26px;
-      grid-area: logo;
-      padding: 10px;
-      align-self: center;
-      & > a,
-      & > a:visited,
-      & > a:hover {
+      display: grid;
+      grid-template-columns: repeat(12, 1fr);
+      grid-template-rows: auto auto;
+      grid-template-areas:
+      "logo logo logo logo logo logo logo logo logo logo logo mbtn";
+      .logo {
+        background: #222;
         color: white;
-        text-decoration: none;
+        font-size: 26px;
+        grid-area: logo;
+        padding: 10px;
+        align-self: center;
+        & > a,
+        & > a:visited,
+        & > a:hover {
+          color: white;
+          text-decoration: none;
+        }
       }
-    }
-    .menu-btn {
-      background: #333;
-      color: white;
-      cursor: pointer;
-      grid-area: mbtn;
-      padding: 10px;
-      text-align: center;
-      .menu-icon {
-        -webkit-transform: rotate(0deg);
-        -moz-transform: rotate(0deg);
-        -o-transform: rotate(0deg);
-        transform: rotate(0deg);
-        -webkit-transition: .5s ease-in-out;
-        -moz-transition: .5s ease-in-out;
-        -o-transition: .5s ease-in-out;
-        transition: .5s ease-in-out;
-        span {
-          display: block;
-          position: absolute;
-          height: 3px;
-          width: 50%;
-          background: white;
-          border-radius: 9px;
-          opacity: 1;
-          left: 25%;
+      .menu-btn {
+        background: #333;
+        color: white;
+        cursor: pointer;
+        grid-area: mbtn;
+        padding: 10px;
+        text-align: center;
+        .menu-icon {
           -webkit-transform: rotate(0deg);
           -moz-transform: rotate(0deg);
           -o-transform: rotate(0deg);
           transform: rotate(0deg);
-          -webkit-transition: .25s ease-in-out;
-          -moz-transition: .25s ease-in-out;
-          -o-transition: .25s ease-in-out;
-          transition: .25s ease-in-out;
-          &:nth-child(1) {
-            top: 0px;
-          }
-          &:nth-child(2),
-          &:nth-child(3) {
-            top: 10px;
-          }
-          &:nth-child(4) {
-            top: 20px;
-          }
-        }
-        &.open {
+          -webkit-transition: .5s ease-in-out;
+          -moz-transition: .5s ease-in-out;
+          -o-transition: .5s ease-in-out;
+          transition: .5s ease-in-out;
           span {
+            display: block;
+            position: absolute;
+            height: 3px;
+            width: 50%;
+            background: white;
+            border-radius: 9px;
+            opacity: 1;
+            left: 25%;
+            -webkit-transform: rotate(0deg);
+            -moz-transform: rotate(0deg);
+            -o-transform: rotate(0deg);
+            transform: rotate(0deg);
+            -webkit-transition: .25s ease-in-out;
+            -moz-transition: .25s ease-in-out;
+            -o-transition: .25s ease-in-out;
+            transition: .25s ease-in-out;
             &:nth-child(1) {
-              top: 10px;
-              width: 0%;
-              left: 50%;
+              top: 0px;
             }
-            &:nth-child(2) {
-              -webkit-transform: rotate(45deg);
-              -moz-transform: rotate(45deg);
-              -o-transform: rotate(45deg);
-              transform: rotate(45deg);
-            }
+            &:nth-child(2),
             &:nth-child(3) {
-              -webkit-transform: rotate(-45deg);
-              -moz-transform: rotate(-45deg);
-              -o-transform: rotate(-45deg);
-              transform: rotate(-45deg);
+              top: 10px;
             }
             &:nth-child(4) {
-              top: 10px;
-              width: 0%;
-              left: 50%;
+              top: 20px;
+            }
+          }
+          &.open {
+            span {
+              &:nth-child(1) {
+                top: 10px;
+                width: 0%;
+                left: 50%;
+              }
+              &:nth-child(2) {
+                -webkit-transform: rotate(45deg);
+                -moz-transform: rotate(45deg);
+                -o-transform: rotate(45deg);
+                transform: rotate(45deg);
+              }
+              &:nth-child(3) {
+                -webkit-transform: rotate(-45deg);
+                -moz-transform: rotate(-45deg);
+                -o-transform: rotate(-45deg);
+                transform: rotate(-45deg);
+              }
+              &:nth-child(4) {
+                top: 10px;
+                width: 0%;
+                left: 50%;
+              }
+            }
+          }
+        }
+        .menu-label {
+          font-size: 18px;
+          margin-top: 30px;
+          text-transform: uppercase;
+          pointer-events: none;
+        }
+      }
+    }
+    .main-nav {
+      display: none;
+      position: sticky;
+      height: 100vh;
+      width: 100vw;
+      align-items: center;
+      justify-content: center;
+      background: #222;
+      &.open {
+        display: flex;
+      }
+      ul {
+        justify-self: center;
+        list-style-type: none;
+        li {
+          margin-bottom: 55px;
+          text-align: center;
+          a {
+            color: white;
+            font-size: 32px;
+            text-decoration: none;
+            &:hover {
+
             }
           }
         }
       }
-      .menu-label {
-        font-size: 18px;
-        margin-top: 30px;
-        text-transform: uppercase;
-        pointer-events: none;
-      }
     }
   }
-  .main-nav {
-    display: none;
-    position: sticky;
-    height: 100vh;
-    width: 100vw;
-    align-content: center;
-    justify-content: center;
-    &.open {
-      display: block;
-    }
-  }
+
 </style>
