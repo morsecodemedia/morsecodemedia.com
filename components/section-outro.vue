bc3a053b542c533c018a3b07d46c4aadb0c770f3
Brandon Morse <brandon@morsecodemedia.com>
2019-01-31 11:00:39 -0500

    created section outro component

---
+++ components/section-outro.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="section-outro">
+    <h4
+      v-html="outro.header"/>
+    <div
+      v-html="outro.body" />
+  </section>
+</template>
+
+<script>
+
+  export default {
+    name: 'SectionOutro',
+    props: {
+      outro: {
+        type: Object,
+        required: true,
+        twoWay: true,
+        default: () => {}
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+</style>
