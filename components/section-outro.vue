37121265db7d6607d75a21da52e4e423e2436b08
Brandon Morse <brandon@morsecodemedia.com>
2019-02-03 15:16:51 -0500

    added some rough styles to section outro component. TODO: determine something better for this

--- components/section-outro.vue
+++ components/section-outro.vue
@@ -2,7 +2,7 @@
   <section class="section-outro">
     <h4
       v-html="outro.header"/>
-    <div
+    <p
       v-html="outro.body" />
   </section>
 </template>
@@ -23,4 +23,40 @@
 </script>
 
 <style lang="scss">
+  .section-outro{
+    background: rgba(147,206,222,1);
+    background: -moz-radial-gradient(center, ellipse cover, rgba(147,206,222,1) 0%, rgba(117,189,209,1) 41%, rgba(73,165,191,1) 100%);
+    background: -webkit-gradient(radial, center center, 0px, center center, 100%, color-stop(0%, rgba(147,206,222,1)), color-stop(41%, rgba(117,189,209,1)), color-stop(100%, rgba(73,165,191,1)));
+    background: -webkit-radial-gradient(center, ellipse cover, rgba(147,206,222,1) 0%, rgba(117,189,209,1) 41%, rgba(73,165,191,1) 100%);
+    background: -o-radial-gradient(center, ellipse cover, rgba(147,206,222,1) 0%, rgba(117,189,209,1) 41%, rgba(73,165,191,1) 100%);
+    background: -ms-radial-gradient(center, ellipse cover, rgba(147,206,222,1) 0%, rgba(117,189,209,1) 41%, rgba(73,165,191,1) 100%);
+    background: radial-gradient(ellipse at center, rgba(147,206,222,1) 0%, rgba(117,189,209,1) 41%, rgba(73,165,191,1) 100%);
+    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#93cede', endColorstr='#49a5bf', GradientType=1 );
+
+    color: #FFF;
+    padding: 80px 125px 80px 40px;
+    h4 {
+      border-bottom: 4px solid #FFF;
+      display: inline-block;
+      font-size: 32px;
+      margin: 0 0 45px 0;
+      padding: 0 75px 15px 0;
+      text-transform: uppercase;
+    }
+    p {
+      font-size: 26px;
+    }
+  }
+
+  @media (min-width: 768px) {
+
+  }
+
+  @media (min-width: 992px) {
+
+  }
+
+  @media (min-width: 1200px) {
+
+  }
 </style>
