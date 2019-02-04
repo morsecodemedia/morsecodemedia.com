6ea130bcbf59816dfc48368011b294b39225d483
Brandon Morse <brandon@morsecodemedia.com>
2019-02-04 15:37:17 -0500

    removed ugly lime green

--- pages/case-studies/index.vue
+++ pages/case-studies/index.vue
@@ -46,15 +46,6 @@
 </script>
 
 <style lang="scss" scoped>
-  .section-hero {
-    background: rgba(210,255,82,1);
-    background: -moz-radial-gradient(center, ellipse cover, rgba(210,255,82,1) 0%, rgba(145,232,66,1) 100%);
-    background: -webkit-gradient(radial, center center, 0px, center center, 100%, color-stop(0%, rgba(210,255,82,1)), color-stop(100%, rgba(145,232,66,1)));
-    background: -webkit-radial-gradient(center, ellipse cover, rgba(210,255,82,1) 0%, rgba(145,232,66,1) 100%);
-    background: -o-radial-gradient(center, ellipse cover, rgba(210,255,82,1) 0%, rgba(145,232,66,1) 100%);
-    background: -ms-radial-gradient(center, ellipse cover, rgba(210,255,82,1) 0%, rgba(145,232,66,1) 100%);
-    background: radial-gradient(ellipse at center, rgba(210,255,82,1) 0%, rgba(145,232,66,1) 100%);
-    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#d2ff52', endColorstr='#91e842', GradientType=1 );
-  }
+
 </style>
 
