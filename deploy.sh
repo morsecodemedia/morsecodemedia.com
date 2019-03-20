e9bcfe046c4514eae210b7ceefefb2e402041516
James Tomasino <james@tomasino.org>
2019-03-20 15:44:58 -0400

    adding deploy script

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+branch="$(git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')"
+if [ "$branch" = "master" ]; then
+  read -r -p "Deploy to production? [y/N]" input
+  case "$input" in
+    [yY][eE][sS]|[yY]) 
+      if yarn build; then
+        rsync -rvhe ssh --progress --delete --chmod=u+rw,g+rw,o+r --exclude-from 'exclude-list.txt' ./ morsecodemedia:/var/www/www.morsecodemedia.com/
+      else
+        echo "Failed to build site. Aborting"
+      fi
+      ;;
+    *)
+      exit 0
+      ;;
+  esac
+else
+  read -r -p "Deploy to stage? [y/N]" input
+  case "$input" in
+    [yY][eE][sS]|[yY]) 
+      if yarn build; then
+        rsync -rvhe ssh --progress --delete --chmod=u+rw,g+rw,o+r --exclude-from 'exclude-list.txt' ./ morsecodemedia:/var/www/stage.morsecodemedia.com/
+      else
+        echo "Failed to build site. Aborting"
+      fi
+      ;;
+    *)
+      exit 0
+      ;;
+  esac
+fi
