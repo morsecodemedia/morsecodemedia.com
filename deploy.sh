adec49087ff5bd3d7290c6f9aa4b2ec267ea07ca
Brandon Morse <brandon@morsecodemedia.com>
2019-03-20 16:21:02 -0400

    Updates deploy script to restart pm2 based on which branch is deployed

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,10 @@ branch="$(git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/
 if [ "$branch" = "master" ]; then
   read -r -p "Deploy to production? " input
   case "$input" in
-    [yY][eE][sS]|[yY]) 
+    [yY][eE][sS]|[yY])
       if yarn build; then
         rsync -ae ssh --progress --delete --chmod=u+rw,g+rw,o+r --exclude-from 'exclude-list.txt' ./ morsecodemedia:/var/www/www.morsecodemedia.com/
+        ssh morsecodemedia pm2 restart "morsecodemedia prod"
       else
         echo "Failed to build site. Aborting"
       fi
@@ -17,9 +18,10 @@ if [ "$branch" = "master" ]; then
 else
   read -r -p "Deploy to stage? " input
   case "$input" in
-    [yY][eE][sS]|[yY]) 
+    [yY][eE][sS]|[yY])
       if yarn build; then
         rsync -ae ssh --progress --delete --chmod=u+rw,g+rw,o+r --exclude-from 'exclude-list.txt' ./ morsecodemedia:/var/www/stage.morsecodemedia.com/
+        ssh morsecodemedia pm2 restart "morsecodemedia stage"
       else
         echo "Failed to build site. Aborting"
       fi
