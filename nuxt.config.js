db79dfb0866f1d46aa257cf189759270d60064b1
Brandon Morse <brandon@morsecodemedia.com>
2021-09-07 17:16:53 -0400

    removing static routing, moving back to dynamic routes

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,35 +1,33 @@
 const pkg = require('./package')
 
-import { aerieCaseStudies } from './assets/configs/aerie-case-studies.json'
-import { alexaCaseStudies } from './assets/configs/alexa-case-studies.json'
-import { endoMensHealthCaseStudies } from './assets/configs/emh-case-studies.json'
-import { endoCaseStudies } from './assets/configs/endo-case-studies.json'
-import { entellusCaseStudies } from './assets/configs/entellus-case-studies.json'
-import { geoblueCaseStudies } from './assets/configs/geoblue-case-studies.json'
-import { ikeaCaseStudies } from './assets/configs/ikea-case-studies.json'
-import { inrikiCaseStudies } from './assets/configs/inriki-case-studies.json'
-import { ipipelineCaseStudies } from './assets/configs/ipipeline-case-studies.json'
-import { joeHandCaseStudies } from './assets/configs/joe-hand-case-studies.json'
-import { jspCaseStudies } from './assets/configs/jsp-case-studies.json'
-import { merionRealtyCaseStudies } from './assets/configs/mcr-case-studies.json'
-import { metuchenCaseStudies } from './assets/configs/metuchen-case-studies.json'
-import { patravelCaseStudies } from './assets/configs/patravel-case-studies.json'
-import { radicavaCaseStudies } from './assets/configs/radicava-case-studies.json'
-import { tunnellCaseStudies } from './assets/configs/tunnell-case-studies.json'
-import { usarlCaseStudies } from './assets/configs/usarl-case-studies.json'
-import { walmartCaseStudies } from './assets/configs/walmart-case-studies.json'
-import { xdcCaseStudies } from './assets/configs/xdc-case-studies.json'
-import { xpdCaseStudies } from './assets/configs/xpd-case-studies.json'
-let dynamicRoutes = () => {
-  let caseStudies = aerieCaseStudies.concat(alexaCaseStudies, endoMensHealthCaseStudies, endoCaseStudies, entellusCaseStudies, geoblueCaseStudies, ikeaCaseStudies, inrikiCaseStudies, ipipelineCaseStudies, joeHandCaseStudies, jspCaseStudies, merionRealtyCaseStudies, metuchenCaseStudies, patravelCaseStudies, radicavaCaseStudies, tunnellCaseStudies, usarlCaseStudies, walmartCaseStudies, xdcCaseStudies, xpdCaseStudies)
-  return new Promise(resolve => {
-    resolve(caseStudies.map(el => `case-studies/${el.title.toLowerCase().replace(/[.\s]/g, '-').replace(/[&#,+()$~%'":*?<>{}]/g, '' )}`))
-  })
-}
+// import { aerieCaseStudies } from './assets/configs/aerie-case-studies.json'
+// import { alexaCaseStudies } from './assets/configs/alexa-case-studies.json'
+// import { endoMensHealthCaseStudies } from './assets/configs/emh-case-studies.json'
+// import { endoCaseStudies } from './assets/configs/endo-case-studies.json'
+// import { entellusCaseStudies } from './assets/configs/entellus-case-studies.json'
+// import { geoblueCaseStudies } from './assets/configs/geoblue-case-studies.json'
+// import { ikeaCaseStudies } from './assets/configs/ikea-case-studies.json'
+// import { inrikiCaseStudies } from './assets/configs/inriki-case-studies.json'
+// import { ipipelineCaseStudies } from './assets/configs/ipipeline-case-studies.json'
+// import { joeHandCaseStudies } from './assets/configs/joe-hand-case-studies.json'
+// import { jspCaseStudies } from './assets/configs/jsp-case-studies.json'
+// import { merionRealtyCaseStudies } from './assets/configs/mcr-case-studies.json'
+// import { metuchenCaseStudies } from './assets/configs/metuchen-case-studies.json'
+// import { patravelCaseStudies } from './assets/configs/patravel-case-studies.json'
+// import { radicavaCaseStudies } from './assets/configs/radicava-case-studies.json'
+// import { tunnellCaseStudies } from './assets/configs/tunnell-case-studies.json'
+// import { usarlCaseStudies } from './assets/configs/usarl-case-studies.json'
+// import { walmartCaseStudies } from './assets/configs/walmart-case-studies.json'
+// import { xdcCaseStudies } from './assets/configs/xdc-case-studies.json'
+// import { xpdCaseStudies } from './assets/configs/xpd-case-studies.json'
+// let dynamicRoutes = () => {
+//   let caseStudies = aerieCaseStudies.concat(alexaCaseStudies, endoMensHealthCaseStudies, endoCaseStudies, entellusCaseStudies, geoblueCaseStudies, ikeaCaseStudies, inrikiCaseStudies, ipipelineCaseStudies, joeHandCaseStudies, jspCaseStudies, merionRealtyCaseStudies, metuchenCaseStudies, patravelCaseStudies, radicavaCaseStudies, tunnellCaseStudies, usarlCaseStudies, walmartCaseStudies, xdcCaseStudies, xpdCaseStudies)
+//   return new Promise(resolve => {
+//     resolve(caseStudies.map(el => `case-studies/${el.title.toLowerCase().replace(/[.\s]/g, '-').replace(/[&#,+()$~%'":*?<>{}]/g, '' )}`))
+//   })
+// }
 
 module.exports = {
-  mode: 'universal',
-
   /*
   ** Headers of the page
   */
@@ -72,9 +70,9 @@ module.exports = {
   /*
   ** Generate Dynamic Routes for Static Build
   */
-  generate: {
-    routes: dynamicRoutes
-  },
+  // generate: {
+  //   routes: dynamicRoutes
+  // },
 
   /*
   ** Customize the progress-bar color
